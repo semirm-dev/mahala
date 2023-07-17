098189d919f5c46cfd08176210554b3750bad981
semirmahovkic <semirmahovkic@192.168.1.55>
2023-07-18 00:16:45 +0200

    search

--- app/src/main/java/semir/mahovkic/mahala/ui/candidate/CandidatesScreen.kt
+++ app/src/main/java/semir/mahovkic/mahala/ui/candidate/CandidatesScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +17,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
@@ -23,13 +31,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import semir.mahovkic.mahala.R
@@ -46,26 +61,41 @@ fun CandidatesScreen(
     val pullRefreshState =
         rememberPullRefreshState(uiState.isRefreshing, { viewModel.loadCandidates() })
 
-    Box(Modifier.pullRefresh(pullRefreshState)) {
-        CandidatesList(uiState.candidates) { candidateId ->
-            navController.navigate("${Screens.CandidateDetails.route}/${candidateId}")
-        }
+    val searchBy = remember { mutableStateOf(TextFieldValue("")) }
 
-        PullRefreshIndicator(
-            uiState.isRefreshing,
-            pullRefreshState,
-            Modifier.align(Alignment.TopCenter)
-        )
+    Column {
+        SearchView(searchBy)
+
+        Box(Modifier.pullRefresh(pullRefreshState)) {
+            CandidatesList(uiState.candidates, searchBy.value.text) { candidateId ->
+                navController.navigate("${Screens.CandidateDetails.route}/${candidateId}")
+            }
+
+            PullRefreshIndicator(
+                uiState.isRefreshing,
+                pullRefreshState,
+                Modifier.align(Alignment.TopCenter)
+            )
+        }
     }
 }
 
 @Composable
 fun CandidatesList(
     candidates: List<CandidateUiState>,
+    searchBy: String,
     onCandidateClick: (candidateId: String) -> Unit
 ) {
+    val filtered = mutableListOf<CandidateUiState>()
+
     LazyColumn {
-        items(candidates, key = { it.id }) { candidate ->
+        if (searchBy.isNotBlank()) {
+            filtered.addAll(candidates.filter { it.name.contains(searchBy) || it.party.contains(searchBy) })
+        } else {
+            filtered.addAll(candidates)
+        }
+
+        items(filtered, key = { it.id }) { candidate ->
             CandidateCard(candidate) {
                 onCandidateClick(candidate.id)
             }
@@ -120,4 +150,55 @@ fun CandidateCard(
             )
         }
     }
+}
+
+@Composable
+fun SearchView(state: MutableState<TextFieldValue>) {
+    TextField(
+        value = state.value,
+        onValueChange = { value ->
+            state.value = value
+        },
+        modifier = Modifier.fillMaxWidth(),
+        textStyle = TextStyle(color = Color.White, fontSize = 18.sp),
+        leadingIcon = {
+            Icon(
+                Icons.Default.Search,
+                contentDescription = "",
+                modifier = Modifier
+                    .padding(15.dp)
+                    .size(24.dp)
+            )
+        },
+        trailingIcon = {
+            if (state.value != TextFieldValue("")) {
+                IconButton(
+                    onClick = {
+                        state.value =
+                            TextFieldValue("") // Remove text from TextField when you press the 'X' icon
+                    }
+                ) {
+                    Icon(
+                        Icons.Default.Close,
+                        contentDescription = "",
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .size(24.dp)
+                    )
+                }
+            }
+        },
+        singleLine = true,
+        shape = RectangleShape, // The TextFiled has rounded corners top left and right by default
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color.White,
+            cursorColor = Color.White,
+            leadingIconColor = Color.White,
+            trailingIconColor = Color.White,
+            backgroundColor = MaterialTheme.colorScheme.primary,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent
+        )
+    )
 }
