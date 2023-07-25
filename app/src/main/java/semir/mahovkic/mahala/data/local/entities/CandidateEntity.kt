f4b50c06e2383617d844d4b0fe486feb7e7ccb1b
semirmahovkic <semirmahovkic@192.168.1.15>
2023-07-25 17:25:55 +0200

    remove unnecessary mdoels

--- app/src/main/java/semir/mahovkic/mahala/data/local/entities/CandidateEntity.kt
+++
@@ -1,9 +0,0 @@
-package semir.mahovkic.mahala.data.local.entities
-
-class CandidateEntity(
-    val id: String,
-    var name: String,
-    var profileImg: Int = 0,
-    var party: String = "",
-    var votes: Int = 0
-)
