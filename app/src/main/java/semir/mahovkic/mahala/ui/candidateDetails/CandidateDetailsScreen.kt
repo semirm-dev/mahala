4028d796ec177d2ff27361eecc9baf5daab2c611
semirmahovkic <semirmahovkic@192.168.1.15>
2023-07-26 14:50:08 +0200

    animate votes

--- app/src/main/java/semir/mahovkic/mahala/ui/candidateDetails/CandidateDetailsScreen.kt
+++ app/src/main/java/semir/mahovkic/mahala/ui/candidateDetails/CandidateDetailsScreen.kt
@@ -151,22 +151,12 @@ fun VotesInfo(newVotes: Int) {
             AnimatedContent(
                 targetState = newVotes,
                 transitionSpec = {
-                    // Compare the incoming number with the previous number.
-                    if (targetState > oldVotes.intValue) {
-                        // If the target number is larger, it slides up and fades in
-                        // while the initial (smaller) number slides up and fades out.
-                        (slideInVertically { height -> height } + fadeIn()).togetherWith(
+                    (slideInVertically { height -> height } + fadeIn())
+                        .togetherWith(
                             slideOutVertically { height -> -height } + fadeOut())
-                    } else {
-                        // If the target number is smaller, it slides down and fades in
-                        // while the initial number slides down and fades out.
-                        (slideInVertically { height -> -height } + fadeIn()).togetherWith(
-                            slideOutVertically { height -> height } + fadeOut())
-                    }.using(
-                        // Disable clipping since the faded slide-in/out should
-                        // be displayed out of bounds.
-                        SizeTransform(clip = false)
-                    )
+                        .using(
+                            SizeTransform(clip = false)
+                        )
                 },
                 label = ""
             ) { value ->
